23691beea79278e1b0b89ee17cd36b2bacfb55ca
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-05-31 20:16:40 +0530

    def mostFrequent(arr, n):     arr.sort()
        max_count = 1
        res = arr[0]
        curr_count = 1
     
        for i in range(1, n):
            if (arr[i] == arr[i - 1]):
                curr_count += 1
            else:
                curr_count = 1
     
            if (curr_count > max_count):
                max_count = curr_count
                res = arr[i - 1]
     
        return res
     
    n=int(input())
    arr =list(map(int,input().split()))
    
    print(mostFrequent(arr, n))

---
+++ Most_Frequent.py
@@ -0,0 +1,23 @@
+def mostFrequent(arr, n):
+    arr.sort()
+ 
+    max_count = 1
+    res = arr[0]
+    curr_count = 1
+ 
+    for i in range(1, n):
+        if (arr[i] == arr[i - 1]):
+            curr_count += 1
+        else:
+            curr_count = 1
+ 
+        if (curr_count > max_count):
+            max_count = curr_count
+            res = arr[i - 1]
+ 
+    return res
+ 
+n=int(input())
+arr =list(map(int,input().split()))
+
+print(mostFrequent(arr, n))
