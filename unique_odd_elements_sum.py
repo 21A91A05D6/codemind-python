d06df875eabb6e23828d626f12905360e07690dc
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-06-04 09:19:27 +0530

    n=int(input()) a=list(map(int,input().split())) temp=[] sum=0 for element in a: #elmt there in a     if(element not in temp):  #not in         temp.append(element)     sum=0     for element in temp:         if element%2!=0:             sum+=element             continue print(sum)

---
+++ unique_odd_elements_sum.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+temp=[]
+sum=0
+for element in a: #elmt there in a
+    if(element not in temp):  #not in
+        temp.append(element) 
+    sum=0
+    for element in temp:
+        if element%2!=0:
+            sum+=element
+            continue
+print(sum)
