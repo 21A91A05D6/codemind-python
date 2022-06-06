fd986d63d317395b845458922eb7f460237ed2aa
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-06-06 12:05:52 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) res=[] for element in a:     if(element in b and element not in res):         res.append(element) print(len(res))

---
+++ Count_elements_I.py
@@ -0,0 +1,8 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+res=[]
+for element in a:
+    if(element in b and element not in res):
+        res.append(element)
+print(len(res))
