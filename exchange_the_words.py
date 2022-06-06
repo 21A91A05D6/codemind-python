f146a2b14ccd1baeb3434fe379970bcdffcbf7ea
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-06-06 13:16:43 +0530

    # input string string = input("") # reversing words in a given string s = string.split()[::-1] l = [] for i in s:     # apending reversed words to l     l.append(i) # printing reverse words print(" ".join(l))

---
+++ exchange_the_words.py
@@ -0,0 +1,11 @@
+ 
+# input string
+string = input("")
+# reversing words in a given string
+s = string.split()[::-1]
+l = []
+for i in s:
+    # apending reversed words to l
+    l.append(i)
+# printing reverse words
+print(" ".join(l))
