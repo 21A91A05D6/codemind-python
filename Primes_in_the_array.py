a6ee81947bf9b124879b5be603cdc68df706f5f8
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-07-10 22:13:05 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(0,n):     r=a[i]     if(r==1):         continue     for j in range(2,int(r**0.5)+1):         if r%j==0:             break     else:         c+=1 print(c)

--- Primes_in_the_array.py
+++ Primes_in_the_array.py
@@ -1,14 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
 c=0
-for j in range(0,n):
-    r=a[j]
-    if r==1:
+for i in range(0,n):
+    r=a[i]
+    if(r==1):
         continue
-    for i in range(2,(r//2)+1):
-        if r%i==0:
-           break
+    for j in range(2,int(r**0.5)+1):
+        if r%j==0:
+            break
     else:
         c+=1
 print(c)
-        
